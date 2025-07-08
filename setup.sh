e346bdae3a6b4bf12675c2f3cba1c57c49b8d0a2
Rishi Mailoorkar <86928071+rishimailoorkar@users.noreply.github.com>
2025-07-08 17:18:06 +0530

    Add setup.sh for deployment

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install --no-cache-dir --no-build-isolation --no-use-pep517 git+https://github.com/xinntao/BasicSR.git@8d56e3a045f9fb3e1d8872f92ee4a4f07f886b0a
+pip install -r requirements.txt
